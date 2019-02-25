2e078216509b620ede6801696969530d5c48fc81
Guilherme Gavazzoni <gg_numbuh4@hotmail.com>
2019-02-25 19:07:02 -0300

    Criado as entidades de receita

---
+++ src/Receitas.Framework/Entities/IEntity.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Receitas.Framework.Entities
+{
+    public interface IEntity
+    {
+        long ID { get; set; }
+    }
+}
---
+++ src/Receitas.WebAPI/Entities/PassoPreparo.cs
@@ -0,0 +1,11 @@
+﻿using Receitas.Framework.Entities;
+using System.ComponentModel.DataAnnotations;
+
+namespace Receitas.WebAPI.Entities
+{
+    public class PassoPreparo : BaseEntity
+    {
+        [MaxLength(255)]
+        public string Descricao { get; set; }
+    }
+}
